a1ff851553804cb593cfa24cbaca5107cdca4b25
Conor Hanson <condorhanson@gmail.com>
2022-06-20 14:12:44 -0500

    finished

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -4,16 +4,19 @@ console.log("Yep. It's working. Mapping multiple points engage!!");
 // Add a map object with center and zoom level
 let map = L.map("map").setView([40.7, -94.5], 4);
 
-// Add a marker
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// get data
+let cityData = cities;
 
-// Alternative to above:
-//         let map = L.map("mapid", {
-//         center: [
-//         40.7, -94.5
-//         ],
-//         zoom: 4
-//      });
+// iterate through the cities array and add each location to the marker() function
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000,
+        color: "red",
+        fillColor: "white"})
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population: " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+})
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
